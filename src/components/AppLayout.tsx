aea10426dd1412cb165966e79b993e9e34c0bb23
JustRell <devrellmara@gmail.com>
2024-03-12 00:34:00 -0700

    Made it Responsive

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -3,7 +3,7 @@ import SearchDisplay from "./SearchDisplay"
 
 function AppLayout() {
   return (
-    <div className=" bg-slate-300 p-5 rounded-md border-[2px] min-w-[300px] min-h-[100px] border-dashed border-indigo-500">
+    <div className=" bg-slate-300 p-5 rounded-md border-[2px] min-w-[200px] max-w-[350px] min-h-[100px] border-dashed border-indigo-500">
         <SearchBar /> 
         <SearchDisplay />
     </div>
